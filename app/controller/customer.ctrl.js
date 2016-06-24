9d3fb5c2ae3f2e6d32bb156c3abfe15ef48dbb18
Sajal Rustagi <sajalrustagi1993@gmail.com>
2016-06-24 17:18:40 +0530

    added parse

--- app/controller/customer.ctrl.js
+++ app/controller/customer.ctrl.js
@@ -7,6 +7,6 @@
 	angular.module("customer_module",["upper_directive"]);
 
 	angular.module("customer_module").controller("customerInfoController",function($scope){
-		 $scope.userInfo=localStorage.getItem("user");
+		 $scope.userInfo=JSON.parse(localStorage.getItem("user"));
 	});
 })();
