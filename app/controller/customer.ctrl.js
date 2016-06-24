604bb52551d3b93f38340b9d374135aff34f0922
Pulkit Arora <puarora@puarora-mbp.corp.adobe.com>
2016-06-24 18:31:02 +0530

    fetching menu items

--- app/controller/customer.ctrl.js
+++ app/controller/customer.ctrl.js
@@ -4,9 +4,22 @@
 
 (function(){
 
-	angular.module("customer_module",["upper_directive"]);
+	angular.module("customer_module",["upper_directive","service_module"]);
 
 	angular.module("customer_module").controller("customerInfoController",function($scope){
 		 $scope.userInfo=JSON.parse(localStorage.getItem("user"));
+	});
+
+	angular.module("customer_module").controller("MenuItemsController",function($scope, $routeParams, RestaurantService){
+
+		//$scope.menuItems = [];
+		
+		(function() {
+			RestaurantService.getMenuItems($routeParams.restaurantName).then(function(result){
+				$scope.menuItems = result.data;
+				console.log($scope.menuItems);
+			});
+		})();
+		
 	});
 })();
---
+++ app/service/restaurant.srv.js
@@ -0,0 +1,27 @@
+/**
+* @author:puarora@adobe.com
+* @version: 1.0
+* Service code to interact with REST endpoints
+*/
+
+(function(){
+	angular.module("service_module",[]); //creating module ------ without [] is getter for the module
+
+	angular.module("service_module").service("RestaurantService",function($http,$q){
+		
+		this.getMenuItems = function(name) {
+			var deferred = $q.defer();
+			$http.get("http://localhost:9000/restaurant?name="+name).then(
+				function(data) {
+					deferred.resolve(data);
+				},
+				function(data) {
+					deferred.reject(data);
+				}
+			);
+			return deferred.promise;
+		}
+
+		
+	});
+})();
