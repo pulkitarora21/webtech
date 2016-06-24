03b130be6ba6555cc4cf23fadbdabedaa4fcc895
Pulkit Arora <puarora@puarora-mbp.corp.adobe.com>
2016-06-24 12:59:34 +0530

    Intial setup

---
+++ app/config/main.cfg.js
@@ -0,0 +1,29 @@
+/**
+* @author:puarora@adobe.com
+* @version:1.0
+* Main configuration file for routing 
+*/
+
+(function() {
+	angular.module("main_module",["ngRoute"]);
+
+	angular.module("main_module").config(function($routeProvider,$locationProvider){
+		$routeProvider
+		.when("/",{
+			templateUrl: 'app/page/restaurant.html'
+		})
+		.when("/customer",{
+			templateUrl: 'app/page/customer_details.html'
+		})
+		.when("/menu/:restaurantName",{
+			templateUrl: 'app/page/restaurant_details.html'
+		})
+		.when("/checkout",{
+			templateUrl: 'app/page/checkout.html'
+		})
+		.otherwise({
+			templateUrl: 'app/page/restaurant.html'
+		})
+	})
+	
+})();
