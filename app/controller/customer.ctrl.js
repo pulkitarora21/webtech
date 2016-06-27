95c9f1273d377d79fec1b0cce547dcabb7d7dee7
Pulkit Arora <puarora@puarora-mbp.corp.adobe.com>
2016-06-27 11:07:16 +0530

    changes in cart items addition

--- app/controller/customer.ctrl.js
+++ app/controller/customer.ctrl.js
@@ -22,11 +22,14 @@
 			});
 		})();
 
-		$rootScope.cartItems = {};
-		$rootScope.currentRestaurant= "";
+		// $rootScope.cartItems = {};
+		// $rootScope.currentRestaurant= "";
+		// $rootScope.totalPrice = 0;
 		$scope.addToCart = function(menuItem, restaurantName) {
-			if($rootScope.currentRestaurant==""){
+			if($rootScope.currentRestaurant==undefined){
 				$rootScope.currentRestaurant = restaurantName;
+				$rootScope.cartItems = {};
+				$rootScope.totalPrice = 0;
 			}
 
 			if(restaurantName!=$rootScope.currentRestaurant && Object.keys($rootScope.cartItems).length>0) {
@@ -35,10 +38,13 @@
 			else {
 				if($rootScope.cartItems[menuItem.name]==undefined) {
 					$rootScope.cartItems[menuItem.name] = 1;
+					$rootScope.totalPrice += menuItem.price;
 				}
 				else {
 					$rootScope.cartItems[menuItem.name] += 1;
+					$rootScope.totalPrice += menuItem.price;
 				}
+				$rootScope.totalPrice = Math.round($rootScope.totalPrice*100)/100;
 			}
 		}
 
