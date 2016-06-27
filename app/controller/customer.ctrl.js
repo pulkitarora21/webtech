062179f7f30044de1e4a8c445522f8b44a197618
Pulkit Arora <puarora@puarora-mbp.corp.adobe.com>
2016-06-27 10:37:26 +0530

    Menu listing and cart done

--- app/controller/customer.ctrl.js
+++ app/controller/customer.ctrl.js
@@ -10,16 +10,39 @@
 		 $scope.userInfo=JSON.parse(localStorage.getItem("user"));
 	});
 
-	angular.module("customer_module").controller("MenuItemsController",function($scope, $routeParams, RestaurantService){
+	angular.module("customer_module").controller("RestaurantDetailsController",function($scope, $rootScope, $routeParams, RestaurantService){
 
 		//$scope.menuItems = [];
 		
 		(function() {
-			RestaurantService.getMenuItems($routeParams.restaurantName).then(function(result){
-				$scope.menuItems = result.data;
-				console.log($scope.menuItems);
+			RestaurantService.getRestaurantDetails($routeParams.restaurantName).then(function(result){
+				$scope.restaurantDetails = result.data[0];
+				$scope.menuitems = $scope.restaurantDetails.menuItems;
+				console.log($scope.restaurantDetails);
 			});
 		})();
+
+		$rootScope.cartItems = {};
+		$rootScope.currentRestaurant= "";
+		$scope.addToCart = function(menuItem, restaurantName) {
+			if($rootScope.currentRestaurant==""){
+				$rootScope.currentRestaurant = restaurantName;
+			}
+
+			if(restaurantName!=$rootScope.currentRestaurant && Object.keys($rootScope.cartItems).length>0) {
+				console.log("Can't add item from this restaurant while cart has items from other restaurants");
+			}
+			else {
+				if($rootScope.cartItems[menuItem.name]==undefined) {
+					$rootScope.cartItems[menuItem.name] = 1;
+				}
+				else {
+					$rootScope.cartItems[menuItem.name] += 1;
+				}
+			}
+		}
+
+
 	});
 
 })();
